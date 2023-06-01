ae83ff320bfc893c8a0f5f145701c9494c10d1e9
David Ra <david.yra762@gmail.com>
2023-06-01 14:23:58 -0500

    added models and admin

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,2 +1,24 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+from django.urls import reverse
+
+
 
 # Create your models here.
+
+class LocationVO(models.Model):
+    href = models.CharField(max_length=300, null=True, blank=True, unique=True)
+    closet_name = models.CharField(max_length=300)
+    section_number = models.PositiveBigIntegerField()
+    shelf_number = models.PositiveBigIntegerField()
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    picture_url = models.URLField(null=True, blank=True)
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.PROTECT,
+    )
